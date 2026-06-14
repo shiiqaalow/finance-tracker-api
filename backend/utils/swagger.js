29c526857a9fabb7dff65d83331a9a5e671ab841
Abdimalik Mohamed <shiiqaalow99@gmail.com>
2026-06-14 15:50:52 +0200

    changed swagger title in  swagger file

--- backend/utils/swagger.js
+++ backend/utils/swagger.js
@@ -5,7 +5,7 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Task Manager API',
+      title: 'Finance Tracker API',
       version: '1.0.0',
       description: 'API documentation for our finance-tracker backend'
     },
